eb07c03d72f2de07cbc0dc6117365bfa06deddf9
Will Luft <william.d.luft@gmail.com>
2024-08-21 10:20:35 -0400

    Exercise 3: Adding metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import {
-  Work_Sans,
-  Spline_Sans_Mono,
-} from 'next/font/google';
+import { Work_Sans, Spline_Sans_Mono } from 'next/font/google';
 import clsx from 'clsx';
 
-import { LIGHT_TOKENS, DARK_TOKENS } from '@/constants';
+import { BLOG_TITLE, LIGHT_TOKENS, DARK_TOKENS } from '@/constants';
 
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
@@ -24,13 +21,18 @@ const monoFont = Spline_Sans_Mono({
   variable: '--font-family-mono',
 });
 
+export const metadata = {
+  title: BLOG_TITLE,
+  description: 'A wonderful blog about JavaScript',
+};
+
 function RootLayout({ children }) {
   // TODO: Dynamic theme depending on user preference
   const theme = 'light';
 
   return (
     <html
-      lang="en"
+      lang='en'
       className={clsx(mainFont.variable, monoFont.variable)}
       data-color-theme={theme}
       style={theme === 'light' ? LIGHT_TOKENS : DARK_TOKENS}
