51dcd1190722af35ff9a585aca8cb3ca77570c08
Will Luft <william.d.luft@gmail.com>
2024-08-21 10:24:23 -0400

    Exercise 4: Code snippets with Bright

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -3,7 +3,7 @@ import { MDXRemote } from 'next-mdx-remote/rsc';
 import { BLOG_TITLE } from '@/constants';
 import { loadBlogPost } from '@/helpers/file-helpers';
 import BlogHero from '@/components/BlogHero';
-
+import CodeSnippet from '@/components/CodeSnippet';
 import styles from './postSlug.module.css';
 
 export async function generateMetadata({ params }) {
@@ -26,7 +26,12 @@ async function BlogPost({ params }) {
         publishedOn={frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <MDXRemote source={content} />
+        <MDXRemote
+          source={content}
+          components={{
+            pre: CodeSnippet,
+          }}
+        />
       </div>
     </article>
   );
