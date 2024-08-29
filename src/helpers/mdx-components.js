2d10883cc8842387beb8557f192e79bf9c276fc6
Will Luft <william.d.luft@gmail.com>
2024-08-29 08:53:23 -0400

    Exercise 6, Circular colors widget

--- src/helpers/mdx-components.js
+++ src/helpers/mdx-components.js
@@ -1,9 +1,11 @@
 import DivisionGroupsDemo from '@/components/DivisionGroupsDemo';
 import CodeSnippet from '@/components/CodeSnippet';
+import CircularColorsDemo from '@/components/CircularColorsDemo';
 
 const COMPONENT_MAP = {
   pre: CodeSnippet,
   DivisionGroupsDemo,
+  CircularColorsDemo,
 };
 
 export default COMPONENT_MAP;
